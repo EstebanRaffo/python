3741e591023d656ed882a688f26454e2ff53d0c1
Esteban Raffo <e.fraffo@gmail.com>
2021-09-25 17:47:18 -0300

    25/09

--- python_intro/funciones.py
+++ python_intro/funciones.py
@@ -1,5 +1,4 @@
-# Funcion lambda
-
+# lambda
 resultado = lambda numero : numero + 30
 print(resultado(10))
 
@@ -8,3 +7,42 @@ print(resultado2(5, 8))
 
 promedio = lambda num1, num2, num3 : (num1 + num2 + num3) / 3
 print(promedio(3, 4, 7))
+
+
+# range
+def pares(maximo):
+    for numero in range(maximo):
+        if numero % 2 == 0:
+            yield numero
+
+maximo = 11
+for numero in pares(maximo):
+    print(numero)
+
+
+# Filter
+def positivo(numero):
+    return numero > 0
+
+print(positivo(5))
+print(positivo(-1))
+
+numeros = [4, -2, 3, -5, 7, -6]
+filtro = filter(positivo, numeros)
+resultado = list(filtro)
+print(resultado)
+
+
+# Map
+def multiplicar(numero):
+    return numero * 2
+
+print(multiplicar(2))
+
+numeros = [2, 4, 6]
+mapeo = map(multiplicar, numeros)
+resultado = list(mapeo)
+print(resultado)
+
+lista_resultado = list(map(lambda numero : numero * 2, numeros))
+print(lista_resultado)
