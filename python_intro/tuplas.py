3c2c01e1b85de04ace35fe5b9ddea4274e384258
eraffo <eraffo@sistemasesco.com>
2021-12-14 15:42:50 -0300

    14/12

--- python_intro/tuplas.py
+++ python_intro/tuplas.py
@@ -1,6 +1,11 @@
 # No es modificable como una lista
 
 tupla_colores = ("celeste", "blanco", "negro")
+tupla_A = ("Fondo10", "A")
+tupla_B = ("Fondo10", "A")
+
+print("¿Son iguales las tuplas? ", tupla_A == tupla_B)
+
 print(tupla_colores)
 print(tupla_colores[1])
 print(len(tupla_colores))
