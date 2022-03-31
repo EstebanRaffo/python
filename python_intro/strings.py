505dfdd4194bdbeb199bc1f01dcd37a5db04878d
eraffo <eraffo@sistemasesco.com>
2022-03-31 18:02:54 -0300

    30/03

--- python_intro/strings.py
+++ python_intro/strings.py
@@ -1,15 +1,17 @@
+from idlelib.iomenu import encoding
+
 print("Intro Python")
 
 # Strings
 
-player = input("enter your player: ")
-print("favorite player: " + player)
-
-yourAge = input("Age: ")
-birthYaner = 2021 - int(yourAge)
-print(birthYaner)
-
-
+# player = input("enter your player: ")
+# print("favorite player: " + player)
+#
+# yourAge = input("Age: ")
+# birthYaner = 2021 - int(yourAge)
+# print(birthYaner)
+#
+#
 numero = 5
 # print("El numero es: " + numero)
 print("El numero es: " + str(numero))
@@ -31,27 +33,58 @@ print(len(cadena))
 word = "This is a sentence"
 print(word.upper())
 print(word.lower())
-print(word.find('s'))
-print(word.replace("sentence", "number"))
-print(word.find("sentence"))
-word1 = word.__add__(" and word added")
-print(word1)
-print(word1.__contains__("and"))
-print(word.split())
+print(word.find('y'))
+# print(word.replace("sentence", "number"))
+# print(word.find("sentence"))
+# word1 = word.__add__(" and word added")
+# print(word1)
+# print(word1.__contains__("and"))
+# print(word.split())
+#
+# word = 'hola,que,tal,mundo'
+# print(word.split(','))
+#
+#
+# nombre = 'Antonio'
+# edad = 25
+# print("Hola {} feliz cumpleaños {}".format(nombre, edad))
+#
+# resultado = 10/3
+# print("El resultado es {r}".format(r=resultado))
+# print("El resultado es {r:1.2f}".format(r=resultado))
+#
+# # f-strings
+# nombre = 'Antonio'
+# edad = 25
+# print(f"Buenos dias {nombre}, feliz {edad} cumpleaños")
 
-word = 'hola,que,tal,mundo'
-print(word.split(','))
+
+print("a".isalnum())
+print("a".isalpha())
+print("a".isascii())
+print("a".encode('utf-8'))
+print(type("Ã³".encode()))
+print(type("á".encode()))
 
 
-nombre = 'Antonio'
-edad = 25
-print("Hola {} feliz cumpleaños {}".format(nombre, edad))
+import re
 
-resultado = 10/3
-print("El resultado es {r}".format(r=resultado))
-print("El resultado es {r:1.2f}".format(r=resultado))
 
-# f-strings
-nombre = 'Antonio'
-edad = 25
-print(f"Buenos dias {nombre}, feliz {edad} cumpleaños")
+txt = "Está Anulado"
+txt1 = "EstÃ¡ Anulado"
+txt2 = "NÃºmero"
+txt3 = "DescriÃ³ciÃ³n"
+
+x = re.findall("Ã", txt)
+y = re.findall("Ã", txt1)
+z = re.findall("Ã", txt2)
+w = re.findall("Ã", txt3)
+
+print(txt.find('Ã'))
+print(txt2.find('Ã'))
+
+print(f"Se encontró {len(x)} coincidencias")
+print(f"Se encontró {len(y)} coincidencias")
+print(f"Se encontró {len(z)} coincidencias")
+print(f"Se encontró {len(w)} coincidencias")
+
