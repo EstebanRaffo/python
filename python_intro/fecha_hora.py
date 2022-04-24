9fe189fa6861465641a411798769d765f4189936
Esteban Raffo <e.fraffo@gmail.com>
2022-04-24 12:31:01 -0300

    fecha_hora.py

--- python_intro/fecha_hora.py
+++ python_intro/fecha_hora.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, date
 
 fechaYHora = datetime.now()
 
@@ -15,3 +15,13 @@ seg = fechaYHora.second
 print("Fecha: {}/{}/{}".format(dia, mes, año))
 print("Hora: {}:{}:{}".format(hora, min, seg))
 print("Año: {}".format(año))
+
+dia_semana = fechaYHora.weekday()
+print(dia_semana)
+print(fechaYHora.isoweekday())
+
+def esFinDeSemana():
+    return date.today().weekday() > 4
+
+if esFinDeSemana():
+    print("conprar criptos")
